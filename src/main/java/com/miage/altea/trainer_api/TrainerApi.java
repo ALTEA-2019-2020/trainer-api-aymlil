c23cfb9c758cb8d9eb70799a993d6ad3b0fda1a0
ANTONELLI <aymerick.antonelli@cgi.com>
2020-03-21 14:37:19 +0100

    Correction OK

--- src/main/java/com/miage/altea/trainer_api/TrainerApi.java
+++ src/main/java/com/miage/altea/trainer_api/TrainerApi.java
@@ -9,9 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+@EnableSwagger2
 @SpringBootApplication
 public class TrainerApi {
     public static void main(String... args){
--- src/main/java/com/miage/altea/trainer_api/controller/TrainerController.java
+++ src/main/java/com/miage/altea/trainer_api/controller/TrainerController.java
@@ -3,10 +3,7 @@ package com.miage.altea.trainer_api.controller;
 import com.miage.altea.trainer_api.bo.Trainer;
 import com.miage.altea.trainer_api.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trainers")
@@ -27,5 +24,10 @@ public class TrainerController {
     @GetMapping("/{name}")
     Trainer getTrainer(@PathVariable String name){
         return this.trainerService.getTrainer(name);
+    }
+
+    @PostMapping("/")
+    public Trainer createTrainer(@RequestBody Trainer trainer){
+        return trainerService.createTrainer(trainer);
     }
 }
